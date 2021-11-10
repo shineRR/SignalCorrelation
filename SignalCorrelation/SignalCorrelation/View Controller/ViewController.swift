b99ce6aa2318c1e97c9db77a7ef867f560235d36
Ilya Baryko <barykoilyass@gmail.com>
2021-11-10 20:21:59 +0300

    Init project

---
+++ SignalCorrelation/SignalCorrelation/View Controller/ViewController.swift
@@ -0,0 +1,28 @@
+//
+// ViewController.swift
+// SignalCorrelation
+//
+// Created by Ilya Baryko on 10.11.21.
+// 
+//
+
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
